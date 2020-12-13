2d58f169c45cfd064dde869c765e9eeb334a9544
Vixan <vixan.one@gmail.com>
2020-12-13 20:14:33 +0200

    ✨ Create a basic 2D grid with react-konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import { Layer, Rect, Stage } from "react-konva";
+
+const CELL_SIZE = 50;
+const CELL_BORDER_COLOR="#333333";
 
 export const App: FC<{}> = () => {
-  return <div className="w-4 h-4 bg-red-400"></div>;
+  const renderGrid = () => {
+    const cells = [];
+
+    for (let row = 0; row < 12; row++) {
+      for (let col = 0; col < 12; col++) {
+        cells.push({
+          row: row * CELL_SIZE,
+          col: col * CELL_SIZE,
+          key: `cell-${row}-${col}`
+        });
+      }
+    }
+
+    return (
+      <Layer className="border-2">
+        {cells.map(cell => (
+          <Rect
+            x={cell.row}
+            y={cell.col}
+            width={CELL_SIZE}
+            height={CELL_SIZE}
+            key={cell.key}
+            stroke={CELL_BORDER_COLOR}
+            strokeWidth={1}
+          />
+        ))}
+      </Layer>
+    );
+  };
+
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-gray-800">
+      <Stage width={600} height={600} className="p-4 bg-gray-500 shadow-2xl">
+        {renderGrid()}
+
+        <Layer></Layer>
+      </Stage>
+    </div>
+  );
 };
